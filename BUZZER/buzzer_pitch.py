9ce2eb0571c70786a7c148f99b13fc0e3c7760ec
Learning  Embedded <learningembedded.com@gmail.com>
2015-02-05 02:35:42 +0530

    Update buzzer_pitch.py

--- BUZZER/buzzer_pitch.py
+++ BUZZER/buzzer_pitch.py
@@ -14,12 +14,12 @@
 # http://learningembedded.com/
 
 import Adafruit_BBIO.GPIO as GPIO 		# import gpio module
- import time							# import time
- buzzer_pin = P8_12 					# initialize variable
+ import time					# import time
+ buzzer_pin = P8_12 				# initialize variable
  GPIO.setup(buzzer_pin, GPIO.OUT) 		# set pin as output
  def buzz(pitch, duration): 			# define a function
- period = 1.0 / pitch  					# calculate period
- delay = period / 2						# calculate delay
+ period = 1.0 / pitch  				# calculate period
+ delay = period / 2				# calculate delay
  cycles = int(duration * pitch) 		# calculate cycles
  for i in range(cycles): 
 	GPIO.output(buzzer_pin, True) 		# set pin as high
@@ -28,7 +28,7 @@ import Adafruit_BBIO.GPIO as GPIO 		# import gpio module
 	time.sleep(delay)
  while True:
 	pitch_s = raw_input("Enter Pitch (200 to 2000): ")  # input pitch values
-	pitch = float(pitch_s) 								# typecast it to float
+	pitch = float(pitch_s) 				    # typecast it to float
 	duration_s = raw_input("Enter Duration (seconds): ")# enter duration
-	duration = float(duration_s) 						# typecast it to float
-	buzz(pitch, duration)								# call buzz function
+	duration = float(duration_s) 			    # typecast it to float
+	buzz(pitch, duration)				    # call buzz function
